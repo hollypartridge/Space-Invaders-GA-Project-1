0bdb7a8552a6546aeffde0bde65bef8c1f00a0ae
hollypartridge <holpartridge@gmail.com>
2021-11-13 14:32:56 +0000

    aliens added to board in fixed position

--- scripts/app.js
+++ scripts/app.js
@@ -2,12 +2,14 @@
 const grid = document.querySelector('.grid')
 const cells = []
 const startBtn = document.querySelector('#start')
+const aliens = []
 
 // Variables
 const width = 10
 const gridCellCount =  width * width
 
 let playerPosition = 94
+let playerBeamPosition = playerPosition - 10
 
 // The Grid
 function createGrid() {
@@ -16,45 +18,63 @@ function createGrid() {
     cell.textContent = i
     cells.push(cell)
     grid.appendChild(cell)
+    
+    if (i < 50 && i > 9) {
+      aliens.push(cell)
+    }
   }
 }
 
 createGrid()
 
 // Functions
-function removePlayer() {
+
+function createAliens() {
+  aliens.forEach(alien => {
+    return alien.classList.add('alien')
+  })
+}
+
+function removePlayerAndBeam() {
+  cells[playerBeamPosition].classList.remove('playerBeam')
   cells[playerPosition].classList.remove('player')
 }
 
-
-function addPlayer() {
+function addPlayerAndBeam() {
+  cells[playerBeamPosition].classList.add('playerBeam')
   cells[playerPosition].classList.add('player')
 }
 
-function handlePlayerKeyLeftAndRight(e) {
+function handleKeyUp(e) {
   const x = playerPosition % width
-  removePlayer()
+  removePlayerAndBeam()
   switch (e.code) {
     case 'ArrowRight':
       if (x < width - 1) {
         playerPosition++
+        playerBeamPosition++
       }
       break
     case 'ArrowLeft':
       if (x > 0) {
         playerPosition--
+        playerBeamPosition--
       }
       break
+    case 'Space':
+      playerBeamPosition -= 80
+      break
   }
-  addPlayer()
+  addPlayerAndBeam()
 }
 
 function handleStart() {
-  addPlayer()
+  createAliens()
+  addPlayerAndBeam()
 }
 
 
 // Events
 
 startBtn.addEventListener('click', handleStart)
-document.addEventListener('keyup', handlePlayerKeyLeftAndRight)
+document.addEventListener('keyup', handleKeyUp)
