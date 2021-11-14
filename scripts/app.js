a13fff272ca27f2c05e4fec59a5b3b36d0af17c8
hollypartridge <holpartridge@gmail.com>
2021-11-14 13:50:13 +0000

    handlePlayerBeam and aliens added

--- scripts/app.js
+++ scripts/app.js
@@ -10,16 +10,17 @@ const gridCellCount =  width * width
 
 let playerPosition = gridCellCount - (width / 2)
 let playerBeamPosition = playerPosition - width
+let timerId = null
 
-// The Grid
+// The Grid And Aliens
 function createGrid() {
   for (let i = 0; i < gridCellCount; i++) {
     const cell = document.createElement('div')
     cell.textContent = i
     cells.push(cell)
     grid.appendChild(cell)
-    
-    if (i >= (gridCellCount / 10) && i < (width * 5)) {
+
+    if (i >= 42 && i < 58 || i >= 62 && i < 78 || i >= 82 && i < 98 || i >= 102 && i < 118 || i >= 122 && i < 138) {
       aliens.push(cell)
     }
   }
@@ -28,26 +29,33 @@ function createGrid() {
 createGrid()
 
 // Functions
+
+function handleStart() {
+  addAliens()
+  addPlayerBeam()
+  addPlayer()
+}
+
 function addAliens() {
   aliens.forEach(alien => {
     return alien.classList.add('alien')
   })
 }
 
-function removePlayerBeam() {
-  cells[playerBeamPosition].classList.remove('playerBeam')
-}
-
-function addPlayerBeam() {
-  cells[playerBeamPosition].classList.add('playerBeam')
+function addPlayer() {
+  cells[playerPosition].classList.add('player')
 }
 
 function removePlayer() {
   cells[playerPosition].classList.remove('player')
 }
 
-function addPlayer() {
-  cells[playerPosition].classList.add('player')
+function addPlayerBeam() {
+  cells[playerBeamPosition].classList.add('playerBeam')
+}
+
+function removePlayerBeam() {
+  cells[playerBeamPosition].classList.remove('playerBeam')
 }
 
 function handleKeyDown(e) {
@@ -67,15 +75,29 @@ function handleKeyDown(e) {
         playerBeamPosition--
       }
       break
+    case 'Space':
+      handlePlayerBeam()
+      break
   }
   addPlayerBeam()
   addPlayer()
 }
 
-function handleStart() {
-  addAliens()
-  addPlayerBeam()
-  addPlayer()
+function handlePlayerBeam() {
+  timerId = window.setInterval(() => {
+    if (playerBeamPosition <= width) {
+      clearInterval(timerId)
+      removePlayerBeam()
+      playerBeamPosition = playerPosition - width
+      addPlayerBeam()
+      return
+    } else if (cells[playerBeamPosition].classList.contains('alien')) {
+      console.log('aliens')
+    }
+    removePlayerBeam()
+    playerBeamPosition -= width
+    addPlayerBeam()
+  },150)
 }
 
 // Events
