3eb42c4fef2c9476867eb3c8d15224c9a95577fb
hollypartridge <holpartridge@gmail.com>
2021-11-16 13:01:36 +0000

    aliens moving no x y axis

--- scripts/app.js
+++ scripts/app.js
@@ -8,14 +8,88 @@ const scoreDisplay = document.querySelector('#score-display')
 // Variables
 const width = 20
 const gridCellCount =  width * width
-const aliens = [42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137]
+const aliens = [{ currentIndex: 42, isAlive: true }, 
+  { currentIndex: 43, isAlive: true }, 
+  { currentIndex: 44, isAlive: true },
+  { currentIndex: 45, isAlive: true },
+  { currentIndex: 46, isAlive: true },
+  { currentIndex: 47, isAlive: true },
+  { currentIndex: 48, isAlive: true },
+  { currentIndex: 49, isAlive: true },
+  { currentIndex: 50, isAlive: true },
+  { currentIndex: 51, isAlive: true },
+  { currentIndex: 52, isAlive: true },
+  { currentIndex: 53, isAlive: true },
+  { currentIndex: 54, isAlive: true },
+  { currentIndex: 55, isAlive: true },
+  { currentIndex: 56, isAlive: true },
+  { currentIndex: 62, isAlive: true }, 
+  { currentIndex: 63, isAlive: true }, 
+  { currentIndex: 64, isAlive: true },
+  { currentIndex: 65, isAlive: true },
+  { currentIndex: 66, isAlive: true },
+  { currentIndex: 67, isAlive: true },
+  { currentIndex: 68, isAlive: true },
+  { currentIndex: 69, isAlive: true },
+  { currentIndex: 70, isAlive: true },
+  { currentIndex: 71, isAlive: true },
+  { currentIndex: 72, isAlive: true },
+  { currentIndex: 73, isAlive: true },
+  { currentIndex: 74, isAlive: true },
+  { currentIndex: 75, isAlive: true },
+  { currentIndex: 76, isAlive: true },
+  { currentIndex: 82, isAlive: true }, 
+  { currentIndex: 83, isAlive: true }, 
+  { currentIndex: 84, isAlive: true },
+  { currentIndex: 85, isAlive: true },
+  { currentIndex: 86, isAlive: true },
+  { currentIndex: 87, isAlive: true },
+  { currentIndex: 88, isAlive: true },
+  { currentIndex: 89, isAlive: true },
+  { currentIndex: 90, isAlive: true },
+  { currentIndex: 91, isAlive: true },
+  { currentIndex: 92, isAlive: true },
+  { currentIndex: 93, isAlive: true },
+  { currentIndex: 94, isAlive: true },
+  { currentIndex: 95, isAlive: true },
+  { currentIndex: 96, isAlive: true },
+  { currentIndex: 102, isAlive: true }, 
+  { currentIndex: 103, isAlive: true }, 
+  { currentIndex: 104, isAlive: true },
+  { currentIndex: 105, isAlive: true },
+  { currentIndex: 106, isAlive: true },
+  { currentIndex: 107, isAlive: true },
+  { currentIndex: 108, isAlive: true },
+  { currentIndex: 109, isAlive: true },
+  { currentIndex: 110, isAlive: true },
+  { currentIndex: 111, isAlive: true },
+  { currentIndex: 112, isAlive: true },
+  { currentIndex: 113, isAlive: true },
+  { currentIndex: 114, isAlive: true },
+  { currentIndex: 115, isAlive: true },
+  { currentIndex: 116, isAlive: true },
+  { currentIndex: 122, isAlive: true }, 
+  { currentIndex: 123, isAlive: true }, 
+  { currentIndex: 124, isAlive: true },
+  { currentIndex: 125, isAlive: true },
+  { currentIndex: 126, isAlive: true },
+  { currentIndex: 127, isAlive: true },
+  { currentIndex: 128, isAlive: true },
+  { currentIndex: 129, isAlive: true },
+  { currentIndex: 130, isAlive: true },
+  { currentIndex: 131, isAlive: true },
+  { currentIndex: 132, isAlive: true },
+  { currentIndex: 133, isAlive: true },
+  { currentIndex: 134, isAlive: true },
+  { currentIndex: 135, isAlive: true },
+  { currentIndex: 136, isAlive: true }]
 
 let playerPosition = gridCellCount - (width / 2)
 let playerBeamPosition = playerPosition - width
 let timerId = null
 let playerHasShot = false
 let score = 0
-let alienIndex = 0 
+// let alienBeamStartPosition =  aliens[Math.floor(Math.random() * aliens.length)]
 
 // The Grid And Aliens
 function createGrid() {
@@ -27,20 +101,51 @@ function createGrid() {
   }
 }
 
-
 createGrid()
 
 // Functions
 
 function handleStart() {
-  addAliens()
+  setTimer()
   addPlayer()
 }
 
 function addAliens() {
   aliens.forEach(alien => {
-    return cells[alien].classList.add('alien')
+    if (alien.isAlive === true) {
+      return cells[alien.currentIndex].classList.add('alien')
+    }
   })
+}
+
+function removeAliens() {
+  aliens.forEach(alien => {
+    return cells[alien.currentIndex].classList.remove('alien')
+  })
+}
+
+
+function moveAliens() {
+  const finalAlien = aliens[aliens.length - 1]
+    
+  if (finalAlien.currentIndex < 139) {
+    aliens.map(alien => {
+      alien.currentIndex = alien.currentIndex + 1
+    })
+  } else if (finalAlien.currentIndex === 139) {
+    aliens.map(alien => {
+      alien.currentIndex = alien.currentIndex + width
+    })
+  }
+}
+
+function setTimer() {
+  timerId = window.setInterval(() => {
+    removeAliens()
+    moveAliens()
+    addAliens()
+    return
+  },500)
 }
 
 function addPlayer() {
@@ -106,15 +211,19 @@ function handlePlayerBeam() {
 }
 
 function killAlien() {
-  // if (aliens.length <= 0) {
+  // if (aliens.isAlive <= 0) {
   //   gameOver()
   // } else {
 
   clearInterval(timerId)
 
-  alienIndex = aliens.indexOf(playerBeamPosition)
-  const removingAliens = aliens.splice(alienIndex, 1)
-  cells[removingAliens].classList.remove('alien')
+  const alienIndex = aliens.find(alien => {
+    return alien.currentIndex === playerBeamPosition
+  })
+
+  alienIndex.isAlive = false
+
+  removeAliens()
 
   score = score + 100
   scoreDisplay.textContent = score
@@ -124,14 +233,34 @@ function killAlien() {
 }
 
 // function handleAlienBeam() {
-
+//   timerId = window.setInterval(() => {
+//     cells[alienBeamStartPosition].classList.remove('alienBeam')
+//     alienBeamPosition += width
+//     cells[alienBeamStartPosition].classList.add('alienBeam')
+//   }, 200) 
 // }
+
+  // timerId = window.setInterval(() => {
+  //   if (alienBeamStartPosition <= gridCellCount) {
+  //     removePlayerBeam()
+  //     clearInterval(timerId)
+  //     alienBeamStartPosition = alienBeamStartPosition + width
+  //     return
+  //   } else if (cells[playerBeamPosition].classList.contains('alien')) {
+  //     playerHasShot = false
+  //     killAlien()
+  //   }
+  //   removePlayerBeam()
+  //   playerBeamPosition -= width
+  //   addPlayerBeam()
+  //   return
+  // },150) 
 
 // function gameOver() {
 
 // }
 
-// Events
+// Events 
 
 startBtn.addEventListener('click', handleStart)
 document.addEventListener('keydown', handleKeyDown)
